d5c5ba6b34b97d1dd53c6b31efd18ee83eeea328
stevehe-campray <602258720@qq.com>
2017-04-06 09:51:22 +0800

    1.创建项目 submit

---
+++ CoreLabel/CoreLabel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreLabel
+//
+//  Created by hejingjin on 17/4/5.
+//  Copyright © 2017年 teamsun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoreLabel/CoreLabel/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CoreLabel
+//
+//  Created by hejingjin on 17/4/5.
+//  Copyright © 2017年 teamsun. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
